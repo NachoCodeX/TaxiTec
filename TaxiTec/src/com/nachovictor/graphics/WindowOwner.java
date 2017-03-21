16e54ec18f177e12bb64616a0a4ad99d89227f69
NachoCodeX <nachocodex@gmail.com>
2017-03-21 08:54:11 -0600

    # WARNING: head commit changed in the meantime
    #V.0.1

---
+++ TaxiTec/src/com/nachovictor/graphics/WindowOwner.java
@@ -0,0 +1,23 @@
+package com.nachovictor.graphics;
+
+public class WindowOwner extends WindowBase{
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setWindow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void addComponents() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
